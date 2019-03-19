727c91005af605cd1adc32bd4486a940f18f64dc
Dawid Layne <klatkiewiczdawid@gmail.com>
2019-03-19 12:39:31 -0400

    More interesting test

--- src/tests/Game1Game.java
+++ src/tests/Game1Game.java
@@ -3,18 +3,57 @@ package tests;
 import Engine.Bridge;
 
 import java.awt.*;
+import java.util.*;
 
 public class Game1Game extends Bridge {
 
-    public void create() {
+    int x;
+    int y;
+    int vx;
+    int vy;
+    ArrayList bullets;
 
+    public void create() {
+        bullets = new ArrayList();
     }
 
     public void update() {
 
-        g.drawOval(mouseX - 50, mouseY - 50, 100,100);
-        if(key.isPressed('a')) {
-            System.out.println("PRESSED!");
+        class Paint {
+
+            int x;
+            int y;
+            int vx;
+            int vy;
+
+            public Paint(int x, int y, int vx, int vy) {
+                this.x = x;
+                this.y = y;
+                this.vx = vx;
+                this.vy = vy;
+            }
+
+            @Override
+            public String toString() {
+                //lol
+                x += vx;
+                y += vy;
+
+                g.setColor(new Color(255, 91, 67));
+                g.fillOval(x - 10,y - 10,20,20);
+
+                return "";
+            }
+
+        }
+
+        g.drawOval(mouseX - 10, mouseY - 10, 20,20);
+
+        bullets.forEach(Object::toString);
+
+        //shoot
+        if(key.isPressed(' ')) {
+            bullets.add(new Paint(mouseX, mouseY, 0,0));
         }
 
     }
